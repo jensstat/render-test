7093029dfec9eb8b384987ea10e4a56e5bb70a7c
jensstat <jensbergjensen@gmail.com>
2026-02-05 13:06:21 +0100

    tesst

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -33,9 +33,9 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(requestLogger)
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
